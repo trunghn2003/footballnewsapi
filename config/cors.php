3ac5333d121ed516231312ceebae3553ba9a74c7
trunghn2003 <trung@labs.com.vn>
2025-04-01 10:43:36 +0700

    update cors

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['http://157.66.47.51:5173'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
